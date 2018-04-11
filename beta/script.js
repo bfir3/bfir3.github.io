00a31c7fe854a170b6db2f359ad0e44782590664
bfir3 <gone_wild21@hotmail.com>
2018-04-11 00:30:41 -0400

    beta update

--- beta/script.js
+++ beta/script.js
@@ -7,7 +7,8 @@ let db;
 function getUniqueIdentifier() {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
     let r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-    return v.toString(16).split('-')[4];
+	let id = v.toString(16);
+    return id.split('-')[4];
   });
 }
 
