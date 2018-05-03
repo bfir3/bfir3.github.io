863820a28892d53006f3b18e3907e12913d76f07
bfir3 <gone_wild21@hotmail.com>
2018-05-03 03:38:00 -0400

    fix

--- beta/script.js
+++ beta/script.js
@@ -1333,7 +1333,7 @@ function initBuildsBrowser() {
 				query = db.collection("buildTable").where("name", ">", "").startAt(buildBrowserQueryCursor).limit(BUILD_BROWSER_PAGE_LIMIT);
 			}
 			else {
-				query = db.collection("buildTable").where("name", ">", "").startAt(buildBrowserPreviousQueryCursor).limit(BUILD_BROWSER_PAGE_LIMIT);
+				query = db.collection("buildTable").where("name", ">", "").endAt(buildBrowserPreviousQueryCursor).limit(BUILD_BROWSER_PAGE_LIMIT);
 				
 			}
 		  
